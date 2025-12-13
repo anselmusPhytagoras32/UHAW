03a659f6b068c6c7ada86b7e145cce6da153acf3
anselmusPhytagoras32 <gebhel.anselm@gmail.com>
2025-12-14 01:00:53 +0800

    created a main logic for the inventory class

--- src/backend/Inventory.java
+++ src/backend/Inventory.java
@@ -1,7 +1,23 @@
 package backend;
 
 public class Inventory {
-    public void inventory(String itemName, int unitPrice, int itemQuantity){
+    String itemName;
+    int unitPrice, itemQuantity;
 
+    public Inventory(){}
+
+    public Inventory(String itemName, int unitPrice, int itemQuantity){
+        this.itemName = itemName;
+        this.unitPrice = unitPrice;
+        this.itemQuantity = itemQuantity;
+    }
+    public String getItemName(){
+        return itemName;
+    }
+    public int getPrice(){
+        return unitPrice;
+    }
+    public int getItemAmount(){
+        return itemQuantity;
     }
 }
